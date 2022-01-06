9609d72deffc9013b0434e841ae806f34c2aa31b
Wen Chen <peter50710t@gmail.com>
2022-01-06 16:50:39 +0800

    新增alt 輸入框

--- components/Editor/elements/Image.tsx
+++ components/Editor/elements/Image.tsx
@@ -53,24 +53,73 @@ export const Image = ({ attributes, children, element }) => {
     } as React.CSSProperties;
 
     //alt 控制
-    const [alt, setAlt] = React.useState(null);
+    //加入相片時即跳出alt輸入框，且強制輸入
+    const [show, setShow] = React.useState(false);
     React.useEffect(() => {
         if (!element.alt) {
-            let altInput;
-            while (!altInput) {
-                altInput = window.prompt('請輸入alt,此為必填選項');
-            }
-            CustomEditor.setAlt(editor, element.url, altInput, path);
+            // use prompt
+            // let altInput;
+            // while (!altInput) {
+            //     altInput = window.prompt('請輸入alt,此為必填選項');
+            // }
+            // CustomEditor.setAlt(editor, element.url, altInput, path);
+            setShow(true);
         }
     }, []);
-    const editAlt = () => {
-        if (typeof window !== 'undefined') {
-            let altInput;
-            while (!altInput) {
-                altInput = window.prompt('修改Alt內容，不可為空', element.alt);
+    const handleEditAlt = () => {
+        //use prompt
+        // if (typeof window !== 'undefined') {
+        //     let altInput;
+        //     while (!altInput) {
+        //         altInput = window.prompt('修改Alt內容，不可為空', element.alt);
+        //     }
+        //     CustomEditor.setAlt(editor, element.url, altInput, path);
+        // }
+        setShow(true);
+    };
+    //alt 輸入框
+    const InputBox = () => {
+        let altInput = React.useRef(element.alt);
+        const style = {
+            display: show ? 'block' : 'none',
+        } as React.CSSProperties;
+        const handleChange = (e) => {
+            altInput.current = e.target.value;
+        };
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            if (typeof window !== 'undefined' && !altInput.current) {
+                alert('ALT為必填!!!');
+            } else {
+                CustomEditor.setAlt(editor, element.url, altInput.current, path);
+                setShow(false);
             }
-            CustomEditor.setAlt(editor, element.url, altInput, path);
-        }
+        };
+        const handleKeyDown = (e) => {
+            if (e.which === 13) {
+                handleSubmit(e);
+            }
+        };
+        return (
+            <div className='fixed z-50 left-0 top-0 bottom-0 right-0 bg-black bg-opacity-40 ' style={style}>
+                <div className='absolute left-1/2 top-1/3 -translate-x-1/2 transform'>
+                    <div className='grid grid-flow-col  '>
+                        <input
+                            key='input'
+                            autoFocus
+                            placeholder={'請輸入alt'}
+                            className='text-xl text-black rounded-l-md py-1 pl-2  border-none  border-collapse border-r-none outline-none'
+                            onChange={handleChange}
+                            defaultValue={altInput.current}
+                            onKeyDown={handleKeyDown}
+                        />
+                        <div className='bg-green-500 text-white flex p-3 hover:bg-green-600 transform duration-300 cursor-pointer' onClick={handleSubmit}>
+                            <span className='material-icons  justify-center self-center'>done</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     };
 
     return (
@@ -81,9 +130,10 @@ export const Image = ({ attributes, children, element }) => {
                 <DeleButton active onClick={() => Transforms.removeNodes(editor, { at: path })} style={btnSTY}>
                     <Icon>delete</Icon>
                 </DeleButton>
-                <AltButton active onClick={editAlt} style={btnSTY}>
+                <AltButton active onClick={handleEditAlt} style={btnSTY}>
                     <Icon>mode_edit</Icon>
                 </AltButton>
+                <InputBox />
             </div>
         </div>
     );
